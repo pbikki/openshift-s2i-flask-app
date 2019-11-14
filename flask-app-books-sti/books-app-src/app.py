727f9313d4913548e08185ca9ca5813812b27c2c
Poojitha Bikki <50849668+poojitha-bikki@users.noreply.github.com>
2019-11-14 00:41:37 -0600

    Update app.py

--- flask-app-books-sti/books-app-src/app.py
+++ flask-app-books-sti/books-app-src/app.py
@@ -18,7 +18,7 @@ from models import Book
 
 @app.route("/")
 def hello():
-    return jsonify(message="Hello World!")
+    return jsonify(message="Hello World - git webook test!")
 
 
 def _add_book(**kwargs):
